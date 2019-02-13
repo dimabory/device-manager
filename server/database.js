6a4f0993802a395390f3c6222fbfab6c4da36fd9
Dmytro <bbbara10@gmail.com>
2019-02-13 12:06:38 +0200

    Play around connectionString to run CI correctly

--- server/database.js
+++ server/database.js
@@ -3,6 +3,7 @@ const Promise = require('bluebird');
 const pgp = require('pg-promise')({
   promiseLib: Promise,
 });
+
 const config = require('../config/config');
 
-module.exports = pgp(config.db);
+module.exports = pgp({ connectionString: config.db });
